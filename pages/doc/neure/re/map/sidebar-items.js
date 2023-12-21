1ab8e113d629fa880e3cda877ef6ae1e07c3d69c
araraloren <blackcatoverwall@gmail.com>
2023-12-21 18:50:20 +0800

    update doc

--- pages/doc/neure/re/map/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["from_str","from_str_radix","into","select0","select1","select_eq","single","try_into"],"struct":["FromStr","FromStrRadix","MapInto","MapTryInto","Select0","Select1","SelectEq","Single"],"trait":["MapSingle","TryFromStrRadix"]};
