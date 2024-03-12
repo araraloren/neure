19aaffb78daab3e859686adde39fc0df6f18849b
araraloren <blackcatoverwall@gmail.com>
2024-03-12 22:07:34 +0800

    update pages

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"neure":["",[["ctx",[],["guard.rs","policy.rs","regex.rs","span.rs"]],["iter",[],["byte.rs","mod.rs","span.rs"]],["neu",[],["bool.rs","cond.rs","equal.rs","may.rs","op_and.rs","op_not.rs","op_one.rs","op_or.rs","op_repeat.rs","op_then.rs","op_zero.rs","range.rs","units.rs"]],["re",[["ctor",[],["array.rs","boxed.rs","collect.rs","dthen.rs","dynamic.rs","if.rs","ltm.rs","map.rs","opt.rs","or.rs","pad.rs","pat.rs","quote.rs","repeat.rs","sep.rs","slice.rs","then.rs","vec.rs"]],["regex",[],["boxed.rs","dthen.rs","dynamic.rs","literal.rs","not.rs"]]],["ctor.rs","extract.rs","into.rs","null.rs","rec.rs","regex.rs","wrap.rs"]]],["ctx.rs","err.rs","lib.rs","macro.rs","map.rs","neu.rs","re.rs","span.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["neure",["",[["ctx",[],["guard.rs","policy.rs","regex.rs","span.rs"]],["iter",[],["byte.rs","mod.rs","span.rs"]],["neu",[],["bool.rs","cond.rs","equal.rs","may.rs","op_and.rs","op_not.rs","op_one.rs","op_or.rs","op_repeat.rs","op_then.rs","op_zero.rs","range.rs","units.rs"]],["re",[["ctor",[],["array.rs","boxed.rs","collect.rs","dthen.rs","dynamic.rs","if.rs","ltm.rs","map.rs","opt.rs","or.rs","pad.rs","pat.rs","quote.rs","repeat.rs","sep.rs","slice.rs","then.rs","vec.rs"]],["regex",[],["boxed.rs","dthen.rs","dynamic.rs","literal.rs","not.rs"]]],["ctor.rs","extract.rs","into.rs","null.rs","rec.rs","regex.rs","wrap.rs"]]],["ctx.rs","err.rs","lib.rs","macro.rs","map.rs","neu.rs","re.rs","span.rs"]]]\
+]'));
 createSrcSidebar();
